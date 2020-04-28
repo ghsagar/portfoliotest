1563fae2f5c6cc7b7dc44679f58b08ff6af8f0bc
Sagar <ghimiresagar2015@gmail.com>
2020-04-27 21:34:02 -0400

    requirement.txt added

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '__-#e-tz9qk34mvxk%0=qwtfvvar!*!le01%tjdy_w&%y^=30t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ghimiresagar.pythonanywhere.com']
 
@@ -123,5 +123,12 @@ STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+ 
+try:
+    from .local_settings import *
+except ImportError:
+    print("looks like no local file ")
+
+
 
 
